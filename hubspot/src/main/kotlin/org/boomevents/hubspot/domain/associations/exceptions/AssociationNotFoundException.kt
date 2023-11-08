3586cd0d71655454ba8f33bde891fb9ed4fac09f
cmolinatnt <c.molina@tarent.de>
2023-11-08 16:31:33 +0100

    feat: adding proper exception for associations in the case the ids are wrong a non matching object is found

--- hubspot/src/main/kotlin/org/boomevents/hubspot/domain/associations/exceptions/AssociationNotFoundException.kt
+++ hubspot/src/main/kotlin/org/boomevents/hubspot/domain/associations/exceptions/AssociationNotFoundException.kt
@@ -1,9 +1,22 @@
 package org.boomevents.hubspot.domain.deals.exceptions
 
 import org.boomevents.hubspot.domain.associations.exceptions.AssociationException
+import org.boomevents.hubspot.model.mapper.Mapper
 import java.math.BigInteger
 
+data class AssociationErrorDetails(
+    val objectType: String,
+    val objectId: String
+)
+
 class AssociationNotFoundException(
-    associationId: BigInteger,
-    override val message: String = "Association '$associationId' was not found."
-) : AssociationException(message)
+    val objectType: String,
+    val objectId: String
+) : AssociationException("Association error: $objectType with ID $objectId was not found in Hubspot.") {
+    companion object {
+        fun fromJson(json: String): AssociationNotFoundException {
+            val errorDetails = Mapper.mapToObject<AssociationErrorDetails>(json)
+            return AssociationNotFoundException(errorDetails.objectType, errorDetails.objectId)
+        }
+    }
+}
